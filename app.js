54f0d3b87e4a839d2ca0f9fb44c8300deaf1f8d4
JTrue <truehitt@gmail.com>
2023-05-04 11:40:09 -0400

    created add department query

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ const init = async () => {
       viewAllEmployees();
       break;
     case "ADD a DEPARTMENT":
-      //put redirect here;
+      addDepartment();
       break;
     case "ADD a ROLE":
       //put redirect here;
@@ -119,11 +119,53 @@ const viewAllEmployees = async () => {
 // WHEN I choose to add a department
 // THEN I am prompted to enter the name of the department and that department is added to the database
 
+const addDepartment = async () => {
+  const { newDepartment } = await inquirer.prompt({
+    name: 'newDepartment',
+    type: 'input',
+    message: 'What is the name of the new department?',
+  })
+
+  query = `INSERT INTO departments (name) VALUES (?)`;
+  values = newDepartment;
+  db.query(query, values, (err, res) => {
+    if (err) console.log('error', err);
+    console.log('New department added successfully!')
+    viewAllDepartments();
+  })
+}
+
+
 // WHEN I choose to add a role
 // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
+
+
 
 // WHEN I choose to add an employee
 // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
 
+
+
 // WHEN I choose to update an employee role
 // THEN I am prompted to select an employee to update and their new role and this information is updated in the database
+
+
+// BONUS:
+
+// Update employee managers.
+
+
+
+// View employees by manager.
+
+
+
+// View employees by department.
+
+
+
+// Delete departments, roles, and employees.
+
+
+
+// View the total utilized budget of a department—in other words, the combined salaries of all employees in that department.
